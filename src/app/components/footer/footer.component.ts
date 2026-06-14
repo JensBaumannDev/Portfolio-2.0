9e1b63d8bdf4e2bc5f8aef711ba28ccee3d2bce4
JensBaumannDev <baumannjens@outlook.de>
2026-06-14 23:56:04 +0200

    feat/footer_content
    Added footer with full conent
    
    - added socials
    - added avatar
    - added footer links for legal notice and privacy policy
    - added copyright
    - added full responsive to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, TranslatePipe, NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Footer {}
+export class Footer {
+  protected readonly flippedTiles = signal<Record<string, boolean>>({});
+
+  protected toggleFlip(tileId: string): void {
+    this.flippedTiles.update((state) => ({
+      ...state,
+      [tileId]: !state[tileId],
+    }));
+  }
+}
