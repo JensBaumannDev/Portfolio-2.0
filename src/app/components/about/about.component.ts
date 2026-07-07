7a0c53922f9cee449da96bb7360bb0a6b3fbd7cc
JensBaumannDev <baumannjens@outlook.de>
2026-07-07 23:06:48 +0200

    feat: rebuild about and skills sections to new design

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,78 +1,14 @@
-import { Component, ChangeDetectionStrategy, signal, OnInit, OnDestroy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { NgOptimizedImage } from '@angular/common';
-import {
-  lucideBriefcase,
-  lucideInfo,
-  lucideMapPin,
-  lucideTerminal,
-  lucideAward,
-  lucideHammer,
-  lucideGlobe,
-  lucideSparkles,
-  lucideChevronDown,
-  lucideChevronUp
-} from '@ng-icons/lucide';
 import { Reveal } from '../../directives/reveal.directive';
 import { RevealStagger } from '../../directives/reveal-stagger.directive';
 
-const FACT_CARD_COUNT = 9;
-
 @Component({
   selector: 'app-about',
-  imports: [TranslatePipe, NgIcon, NgOptimizedImage, Reveal, RevealStagger],
-  providers: [
-    provideIcons({
-      lucideBriefcase,
-      lucideInfo,
-      lucideMapPin,
-      lucideTerminal,
-      lucideAward,
-      lucideHammer,
-      lucideGlobe,
-      lucideSparkles,
-      lucideChevronDown,
-      lucideChevronUp
-    })
-  ],
+  imports: [TranslatePipe, RouterLink, Reveal, RevealStagger],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class About implements OnInit, OnDestroy {
-  protected readonly images = [
-    './img/profile/hello_world.png',
-    './img/profile/jb_one_small.jpg',
-    './img/profile/jb_two_small.jpg',
-  ];
-
-  protected readonly activeIndex = signal(0);
-  protected readonly flippedStates = signal<boolean[]>(Array(FACT_CARD_COUNT).fill(false));
-  protected readonly isExpanded = signal(false);
-  private intervalId?: ReturnType<typeof setInterval>;
-
-  protected toggleExpand(): void {
-    this.isExpanded.update((val) => !val);
-  }
-
-  ngOnInit(): void {
-    this.intervalId = setInterval(() => {
-      this.activeIndex.update((index) => (index + 1) % this.images.length);
-    }, 8000);
-  }
-
-  protected toggleCard(index: number): void {
-    this.flippedStates.update((states) => {
-      const newStates = Array(FACT_CARD_COUNT).fill(false);
-      newStates[index] = !states[index];
-      return newStates;
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-}
+export class About {}
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,42 +1,27 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Reveal } from '../../directives/reveal.directive';
 import { RevealStagger } from '../../directives/reveal-stagger.directive';
 
-interface Skill {
-  name: string;
-  icon: string;
-  learning?: boolean;
-}
-
 @Component({
   selector: 'app-skills',
-  imports: [TranslatePipe, NgOptimizedImage, Reveal, RevealStagger],
+  imports: [TranslatePipe, Reveal, RevealStagger],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Skills {
-  skills: Skill[] = [
-    { name: 'Angular', icon: 'angular' },
-    { name: 'TypeScript', icon: 'typescript' },
-    { name: 'JavaScript', icon: 'javascript' },
-    { name: 'HTML5', icon: 'html5' },
-    { name: 'CSS3', icon: 'css3' },
-    { name: 'Supabase', icon: 'supabase' },
-    { name: 'Git', icon: 'git' },
-    { name: 'Python', icon: 'python', learning: true },
-    { name: 'Django', icon: 'django', learning: true },
+  protected readonly skills = [
+    'HTML',
+    'CSS',
+    'SCSS',
+    'JavaScript',
+    'TypeScript',
+    'Angular',
+    'Supabase',
+    'REST-APIs',
+    'Git',
+    'Python',
+    'Django',
   ];
-
-  protected readonly flippedStates = signal<boolean[]>(this.skills.map(() => false));
-
-  protected toggleCard(index: number): void {
-    this.flippedStates.update((states) => {
-      const newStates = this.skills.map(() => false);
-      newStates[index] = !states[index];
-      return newStates;
-    });
-  }
 }
